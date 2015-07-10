a524cba1c8ad2aafcd163f2a4222cb4803ae6ca7
binh.nguyenthanh <bin2907@gmail.com>
2015-07-10 17:59:25 +0700

    spring interceptor

---
+++ spring/spring-interceptor/src/main/java/com/bin/framework/spring/config/AppConfig.java
@@ -0,0 +1,37 @@
+package com.bin.framework.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import com.bin.framework.spring.interceptor.ExecuteTimeInterceptor;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "com.bin.framework.spring" })
+public class AppConfig extends WebMvcConfigurerAdapter{
+	
+	public void addInterceptors(InterceptorRegistry registry) {
+	    registry.addInterceptor(executeTimeInterceptor());
+	}
+
+	@Bean
+	public InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	@Bean
+	public ExecuteTimeInterceptor executeTimeInterceptor() {
+		return new ExecuteTimeInterceptor();
+	}
+	
+}
---
+++ spring/spring-interceptor/src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<html>
+<body>
+<h1>Spring MVC Interceptor</h1>
+
+Hello: start time: ${ startTime }  end time: ${ executeTime }
+
+</body>
+</html>
