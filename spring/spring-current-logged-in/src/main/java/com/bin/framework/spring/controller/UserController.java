0c1c87b9fda756e96cc3252127e6091773452ffa
binh.nguyenthanh <bin2907@gmail.com>
2015-07-13 09:54:54 +0700

    spring current logged in

---
+++ spring/spring-current-logged-in/src/main/java/com/bin/framework/spring/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.bin.framework.spring.controller;
+
+import java.security.Principal;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class UserController {
+ 
+    @RequestMapping(value="/userinfo", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model) {
+    	
+    	User user = (User)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String name = user.getUsername();
+   
+        model.addAttribute("username", name);
+    	
+        return "userinfo";
+    }
+    
+    @RequestMapping(value="/userinfo2", method = RequestMethod.GET)
+    public String printWelcome(ModelMap model, Principal principal ) {
+   
+        String name = principal.getName(); //get logged in username
+        model.addAttribute("username", name);
+        return "userinfo";
+   
+    }
+   
+ 
+}
---
+++ spring/spring-current-logged-in/src/main/webapp/WEB-INF/views/userinfo.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>    
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Spring MVC Security Demo</title>
+</head>
+<body>
+	
+	<h1>Logged in User info</h1>
+	
+	${ username }
+	
+</body>
+</html>
