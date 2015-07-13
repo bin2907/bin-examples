2b206b3dd3471e415b85157db705c5fd56845293
binh.nguyenthanh <bin2907@gmail.com>
2015-07-13 10:20:22 +0700

    spring override message

---
+++ spring/spring-override-message/src/main/java/com/bin/framework/spring/config/AppConfig.java
@@ -0,0 +1,38 @@
+package com.bin.framework.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+
+@EnableWebMvc
+@Configuration
+@ComponentScan({ "com.bin.framework.spring" })
+@Import({ SecurityConfig.class })
+public class AppConfig {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+ 
+        return viewResolver;
+    }
+    
+    @Bean(name = "messageSource")
+	public ResourceBundleMessageSource getMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		messageSource.setUseCodeAsDefaultMessage(true);
+		return messageSource;
+	}
+ 
+}
